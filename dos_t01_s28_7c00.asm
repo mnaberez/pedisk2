50415b87de9e199b26fd2ab371236952ee0b08dd
Mike Naberezny <mike@naberezny.com>
2015-02-16 21:51:00 -0800

    Found print calls

--- dos_t01_s28_7c00.asm
+++ dos_t01_s28_7c00.asm
@@ -2,15 +2,14 @@ L7A05 = $7A05
 L7AA3 = $7AA3
 LED3A = $ED3A
 LEE33 = $EE33
-LEFE7 = $EFE7
+puts = $EFE7
 
     *=$7c00
 
     jmp L7C6F
 
-rename_utility:
-    !text $0d,"PEDISK II FILE RENAME UTILITY",$0d
 old_file:
+    !text $0d,"PEDISK II FILE RENAME UTILITY",$0d
     !text "OLD FILE-",0
 new_file:
     !text $0d,"NEW FILE-",0
@@ -20,9 +19,11 @@ not_in_dir:
     !text $0d,"****NOT IN DIRECTORY****",0
 
 L7C6F:
-    lda #<rename_utility
-    ldy #>rename_utility
-    jsr LEFE7
+    ;Print banner and "OLD FILE-"
+    lda #<old_file
+    ldy #>old_file
+    jsr puts
+
     jsr L7AA3
     ldx #$05
 L7C7B:
@@ -32,9 +33,12 @@ L7C7B:
     bpl L7C7B
     lda $7FB1
     sta L7CD8
+
+    ;Print "NEW FILE-"
     lda #<new_file
     ldy #>new_file
-    jsr LEFE7
+    jsr puts
+
     jsr L7AA3
     jsr LEE33
     tax
@@ -63,13 +67,17 @@ L7CB5:
     jsr LED3A
 L7CC0:
     jmp L7A05
+
 L7CC3:
+    ;Print "****NAME ALREADY IN FILE****"
     lda #<already_in_file
 L7CC5:
     ldy #>already_in_file
-    jsr LEFE7
+    jsr puts
     jmp L7A05
+
 L7CCD:
+    ;"****NOT IN DIRECTORY****"
     lda #<not_in_dir
     jmp L7CC5
 
