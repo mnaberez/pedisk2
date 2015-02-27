aa1cb2aa8c6213b8e8a9aa34ad26b3f0f46cac3b
Mike Naberezny <mike@naberezny.com>
2015-02-27 13:32:23 -0800

    Start format program to bootstrap a PEDISK disk

---
+++ bootstrap/format.asm
@@ -0,0 +1,21 @@
+chrout = $ffd2
+
+    *=$0400
+
+bas_header:
+    !byte $00           ;Null byte at start of BASIC program
+    !word bas_eol+1     ;Pointer to the next BASIC line
+bas_line:
+    !word $000a         ;Line number
+    !byte $9e           ;Token for SYS command
+    !text "1037"        ;Arguments for SYS
+bas_eol:
+    !byte $00           ;End of BASIC line
+    !byte $00,$00       ;End of BASIC program
+
+init:
+    lda #'H'
+    jsr chrout
+    lda #'I'
+    jsr chrout
+    rts
