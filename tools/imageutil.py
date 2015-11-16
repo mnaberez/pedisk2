079d85de91bc4f8cd1ea82cfc1e9250a1d2877c2
Mike Naberezny <mike@naberezny.com>
2015-11-15 23:25:13 -0800

    Add class for manipulating PEDISK image data

---
+++ tools/imageutil.py
@@ -0,0 +1,92 @@
+class DiskImage(object):
+    '''Low-level manipulation of PEDISK disk image data.  Seek to track/
+    sector positions with boundary checks and read/write raw data in the
+    image.  This class is abstract.'''
+    SECTOR_SIZE = 128
+    TRACKS = None  # override in subclass
+    SECTORS = None  # override in subclass
+
+    def __init__(self):
+        self.TOTAL_SIZE = self.SECTOR_SIZE * self.SECTORS * self.TRACKS
+        self.data = bytearray(b'\xE5' * self.TOTAL_SIZE)
+        self.home()
+
+    def home(self):
+        '''Seek to the first sector of the first track'''
+        self.seek(track=0, sector=1)
+
+    def seek(self, track, sector):
+        '''Seek to the given track/sector position.  As with the IBM 3740
+        numbering, tracks are 0-based, sectors are 1-based.'''
+        self._validate_ts(track, sector)
+        self.track = 0
+        self.sector = 1
+        self.sector_offset = 0
+        self.data_offset = 0
+        while (self.track != track) or (self.sector != sector):
+            self._incr()
+
+    def write(self, data):
+        '''Write a bytearray of arbitrary length starting at the current
+        position.  The data can span sectors and tracks.  Track/sector is
+        advanced past the data written.  Raises an error if the data
+        exceeds the end of the image.'''
+        wrapped = False
+        for d in data:
+            if wrapped:
+                raise ValueError("Wrote past end of disk")
+            self.data[self.data_offset] = d
+            wrapped = self._incr()
+
+    def read(self, numbytes):
+        '''Read a bytearray of arbitrary length starting at the current
+        position.  The data can span sectors and tracks.  Track/sector is
+        advanced past the data read.  Raises an error if the data exceeds
+        the end of the image.'''
+        wrapped = False
+        data = bytearray()
+        for n in range(numbytes):
+            if wrapped:
+                raise ValueError("Read past end of disk")
+            d = self.data[self.data_offset]
+            data.append(d)
+            wrapped = self._incr()
+        return data
+
+    def _validate_ts(self, track, sector):
+        '''Raise ValueError if track/sector is out of range'''
+        if (track < 0) or (track >= self.TRACKS):
+            msg = 'Track %r not in range 0-%d' % (track, self.TRACKS-1)
+            raise ValueError(msg)
+        if (sector < 1) or (sector > self.SECTORS):
+            msg = 'Sector %r not in range 1-%d' % (sector, self.SECTORS)
+            raise ValueError(msg)
+
+    def _incr(self):
+        '''Increment the current track, sector, and image position pointers
+        by one byte.  Returns True if wrapped around the disk.'''
+        wrapped = False
+        self.data_offset += 1
+        self.sector_offset += 1
+        if self.sector_offset == self.SECTOR_SIZE:
+            self.sector_offset = 0
+            self.sector += 1
+        if self.sector > self.SECTORS:
+            self.track += 1
+            self.sector = 1
+        if self.track == self.TRACKS:
+            wrapped = True
+            self.data_offset = 0
+            self.track = 0
+            self.sector = 1
+        return wrapped
+
+class FiveInchDiskImage(DiskImage):
+    '''5.25" disk'''
+    TRACKS = 41 # tracks numbered 0-40
+    SECTORS = 28 # sectors per track numbered 1-28
+
+class EightInchDiskImage(DiskImage):
+    '''8" disk'''
+    TRACKS = 77 # tracks numbered 0-76
+    SECTORS = 26 # sectors per track numbered 1-26
---
+++ tools/test_imageutil.py
@@ -0,0 +1,164 @@
+import sys
+import unittest
+
+import imageutil
+
+class DiskImageTests(unittest.TestCase):
+
+    # __init__
+
+    def test_ctor_sets_pos_at_start_of_disk(self):
+        img = imageutil.FiveInchDiskImage()
+        self.assertEqual(img.track, 0)
+        self.assertEqual(img.sector, 1)
+        self.assertEqual(img.data_offset, 0)
+        self.assertEqual(img.sector_offset, 0)
+
+    def test_ctor_initalizes_disk_to_0xe5(self):
+        img = imageutil.FiveInchDiskImage()
+        self.assertEqual(len(img.data), img.TOTAL_SIZE)
+        data = bytearray(b'\xe5' * img.TOTAL_SIZE)
+        self.assertEqual(img.read(img.TOTAL_SIZE), data)
+
+    # home
+
+    def test_home_seeks_to_first_sector_of_first_track(self):
+        img = imageutil.FiveInchDiskImage()
+        img.read(5000)
+        self.assertNotEqual(img.track, 0)
+        self.assertNotEqual(img.sector, 1)
+        img.home()
+        self.assertEqual(img.track, 0)
+        self.assertEqual(img.sector, 1)
+        self.assertEqual(img.sector_offset, 0)
+
+    # seek
+
+    def test_seek_raises_for_track_too_low(self):
+        img = imageutil.FiveInchDiskImage()
+        try:
+            img.seek(track=-1, sector=1)
+            self.fail('nothing raised')
+        except ValueError as exc:
+            self.assertEqual(exc.args[0], 'Track -1 not in range 0-40')
+
+    def test_seek_raises_for_track_too_high(self):
+        img = imageutil.FiveInchDiskImage()
+        try:
+            img.seek(track=41, sector=1)
+            self.fail('nothing raised')
+        except ValueError as exc:
+            self.assertEqual(exc.args[0], 'Track 41 not in range 0-40')
+
+    def test_seek_raises_for_sector_too_low(self):
+        img = imageutil.FiveInchDiskImage()
+        try:
+            img.seek(track=0, sector=0)
+            self.fail('nothing raised')
+        except ValueError as exc:
+            self.assertEqual(exc.args[0], 'Sector 0 not in range 1-28')
+
+    def test_seek_raises_for_sector_too_high(self):
+        img = imageutil.FiveInchDiskImage()
+        try:
+            img.seek(track=0, sector=29)
+            self.fail('nothing raised')
+        except ValueError as exc:
+            self.assertEqual(exc.args[0],
+                'Sector 29 not in range 1-28')
+
+    # write
+
+    def test_write_writes_bytes_and_advances_pointers(self):
+        img = imageutil.FiveInchDiskImage()
+        img.seek(track=0, sector=1)
+        img.write(bytearray(b'\x00\x01\x02'))
+        self.assertEqual(img.sector_offset, 3)
+        img.seek(track=0, sector=1)
+        self.assertEqual(bytearray(b'\x00\x01\x02'), img.read(3))
+
+    def test_write_spans_sectors_and_tracks(self):
+        img = imageutil.FiveInchDiskImage()
+        # seek to the last sector of a track
+        img.seek(track=20, sector=28)
+        data = bytearray(b'\x42' * (img.SECTOR_SIZE + 1))
+        img.write(data)
+        # write should have overflowed to first sector of next track
+        self.assertEqual(img.track, 21)
+        self.assertEqual(img.sector, 1)
+        self.assertEqual(img.sector_offset, 1)
+        # seek to first byte of the sector
+        img.seek(track=img.track, sector=img.sector)
+        self.assertEqual(img.read(1)[0], data[-1])
+
+    def test_write_allows_writing_to_very_end_of_disk(self):
+        img = imageutil.FiveInchDiskImage()
+        # fill the last sector on disk but don't overflow
+        img.seek(track=40, sector=28)
+        data = bytearray(b'\x42' * img.SECTOR_SIZE)
+        img.write(data)
+        # track/sector pointer should wrap around
+        self.assertEqual(img.track, 0)
+        self.assertEqual(img.sector, 1)
+        self.assertEqual(img.sector_offset, 0)
+        # verify sector written
+        img.seek(track=40, sector=28)
+        self.assertEqual(img.read(img.SECTOR_SIZE), data)
+
+    def test_write_raises_if_past_end_of_disk(self):
+        img = imageutil.FiveInchDiskImage()
+        # seek to the last sector of a track
+        img.seek(track=40, sector=28)
+        data = bytearray(b'\x42' * (img.SECTOR_SIZE + 1))
+        try:
+            img.write(data)
+        except ValueError as exc:
+            self.assertEqual(exc.args[0], "Wrote past end of disk")
+
+    # read
+
+    def test_read_reads_bytes_and_advances_pointers(self):
+        img = imageutil.FiveInchDiskImage()
+        img.seek(track=0, sector=1)
+        img.write(bytearray(b'\x00\x01\x02'))
+        img.seek(track=0, sector=1)
+        self.assertEqual(bytearray(b'\x00\x01\x02'), img.read(3))
+        self.assertEqual(img.track, 0)
+        self.assertEqual(img.sector, 1)
+        self.assertEqual(img.sector_offset, 3)
+
+    def test_read_spans_sectors_and_tracks(self):
+        img = imageutil.FiveInchDiskImage()
+        # seek to the last sector of a track
+        data = img.read(img.SECTOR_SIZE + 1)
+        # read should have overflowed to first sector of next track
+        expected = bytearray(b'\xe5' * (img.SECTOR_SIZE + 1))
+        self.assertEqual(data, expected)
+
+    def test_read_allows_reading_to_very_end_of_disk(self):
+        img = imageutil.FiveInchDiskImage()
+        # seek to the last track/sector
+        img.seek(track=40, sector=28)
+        img.read(img.SECTOR_SIZE)
+        # track/sector pointer should wrap around
+        self.assertEqual(img.track, 0)
+        self.assertEqual(img.sector, 1)
+        self.assertEqual(img.sector_offset, 0)
+        # verify sector read
+        expected = bytearray(b'\xe5' * img.SECTOR_SIZE)
+        self.assertEqual(img.read(img.SECTOR_SIZE), expected)
+
+    def test_read_raises_if_past_end_of_disk(self):
+        img = imageutil.FiveInchDiskImage()
+        # seek to the last sector of a track
+        img.seek(track=40, sector=28)
+        try:
+            img.read(img.SECTOR_SIZE + 1)
+        except ValueError as exc:
+            self.assertEqual(exc.args[0], "Read past end of disk")
+
+def test_suite():
+    return unittest.findTestCases(sys.modules[__name__])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
