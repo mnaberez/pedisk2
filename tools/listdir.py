7eea0fb91a8963302b5f1b7b1326dfefc6cb75a7
Mike Naberezny <mike@naberezny.com>
2015-11-21 20:59:40 -0800

    Print complete directory information

--- tools/listdir.py
+++ tools/listdir.py
@@ -7,23 +7,53 @@ import sys
 
 import imageutil
 
-if len(sys.argv) != 2:
-    sys.stderr.write(__doc__)
-    sys.exit(1)
-imagename = sys.argv[1]
+def read_image(filename):
+    size = os.path.getsize(imagename)
+    if size == 256256: # 8"
+        img = imageutil.EightInchDiskImage()
+    elif size == 146944: # 5.25"
+        img = imageutil.FiveInchDiskImage()
+    else:
+        raise Exception("Unrecognized image: %r" % imagename)
+    with open(imagename, 'rb') as f:
+        img.data = bytearray(f.read())
+    return img
 
-size = os.path.getsize(imagename)
-if size == 256256: # 8"
-    img = imageutil.EightInchDiskImage()
-elif size == 146944: # 5.25"
-    img = imageutil.FiveInchDiskImage()
-else:
-    sys.stderr.write("Unrecognized image: %r" % imagename)
-    sys.exit(1)
+def print_dir(fs, out=sys.stdout):
+    out.write("Disk Name = %s\n" % fs.diskname.decode('utf-8'))
+    out.write("Next Free TS = %d,%d\n" % fs.next_free_ts)
+    out.write("Bytes Free = %d\n" % fs.num_free_bytes)
+    out.write("Filename Type Load  Entry Trk,Sec #Secs Size\n")
 
-with open(imagename, 'rb') as f:
-    img.data = bytearray(f.read())
+    for entry in fs.read_dir():
+        if not entry.active:
+            continue
 
-fs = imageutil.Filesystem(img)
-for filename in fs.list_dir():
-    print(filename.decode('utf-8'))
+        typename = imageutil.FileTypes.name_of(entry.filetype)
+        if entry.filetype == imageutil.FileTypes.LD:
+            entry_addr = "$%04X" % entry.size
+            size = str(entry.sector_count * fs.image.SECTOR_SIZE)
+        else:
+            entry_addr = " ---"
+            size = str(entry.size)
+
+        cols = (
+            entry.filename.decode("utf-8").ljust(9),
+            typename.ljust(5),
+            "$%04X " % entry.load_address,
+            entry_addr.ljust(6),
+            ("%d,%d" % (entry.track, entry.sector)).ljust(8),
+            str(entry.sector_count).ljust(6),
+            size,
+            )
+        out.write(''.join(cols) + "\n")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write(__doc__)
+        sys.exit(1)
+    imagename = sys.argv[1]
+
+    img = read_image(imagename)
+    fs = imageutil.Filesystem(img)
+    print_dir(fs)
