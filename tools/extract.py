2f430461793bf4c3e29e0112209b0a2dcff9137c
Mike Naberezny <mike@naberezny.com>
2015-11-22 13:32:07 -0800

    Add script to extract all files from an image

---
+++ tools/extract.py
@@ -0,0 +1,48 @@
+'''
+Extract all files from a PEDISK disk image
+Usage: extract.py <image.img>
+'''
+import os
+import sys
+
+import imageutil
+
+def read_image(filename):
+    size = os.path.getsize(imagename)
+    if size == 256256: # 8"
+        img = imageutil.EightInchDiskImage()
+    elif size == 146944: # 5.25"
+        img = imageutil.FiveInchDiskImage()
+    else:
+        raise Exception("Unrecognized image: %r" % imagename)
+    with open(imagename, 'rb') as f:
+        img.data = bytearray(f.read())
+    return img
+
+def modern_filename(entry):
+    f = entry.filename.decode('utf-8').strip()
+    f += '.' + imageutil.FileTypes.name_of(entry.filetype)
+    f = f.replace('*', '_').replace('?', '_').lower()
+    return f
+
+def extract_files(fs, output_dir):
+    for entry in fs.read_dir():
+        if not entry.active:
+            continue
+        data = fs.read_file(entry.filename)
+
+        filename = os.path.join(output_dir, modern_filename(entry))
+        with open(filename, 'wb') as f:
+            f.write(data)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write(__doc__)
+        sys.exit(1)
+    imagename = sys.argv[1]
+
+    img = read_image(imagename)
+    dirname = os.path.splitext(os.path.basename(imagename))[0]
+    os.mkdir(dirname)
+    fs = imageutil.Filesystem(img)
+    extract_files(fs, dirname)
