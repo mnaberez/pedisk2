7918eb9ba241773863497444e0526e05431116f9
Mike Naberezny <mike@naberezny.com>
2015-11-25 11:25:04 -0800

    Rewrite script into functions

--- tools/convert.py
+++ tools/convert.py
@@ -7,46 +7,58 @@ import sys
 
 import imageutil
 
-if len(sys.argv) != 4:
-    sys.stderr.write(__doc__)
-    sys.exit(1)
-srcname, desttype, destname = sys.argv[1:]
+def read_image(filename):
+    size = os.path.getsize(filename)
+    if size == 256256: # 8"
+        img = imageutil.EightInchDiskImage()
+    elif size == 146944: # 5.25"
+        img = imageutil.FiveInchDiskImage()
+    else:
+        raise Exception("Unrecognized image: %r" % filename)
+    with open(filename, 'rb') as f:
+        img.data = bytearray(f.read())
+    return img
 
-# read source image
-size = os.path.getsize(srcname)
-if size == 256256: # 8"
-    srcimg = imageutil.EightInchDiskImage()
-elif size == 146944: # 5.25"
-    srcimg = imageutil.FiveInchDiskImage()
-else:
-    sys.stderr.write("Unrecognized image: %r" % srcname)
-    sys.exit(1)
-with open(srcname, 'rb') as f:
-    srcimg.data = f.read()
-srcfs = imageutil.Filesystem(srcimg)
+def make_image(physical_size):
+    if str(physical_size)[0] == '8': # 8"
+        img = imageutil.EightInchDiskImage()
+    elif str(physical_size)[0] == '5': # 5.25"
+        img = imageutil.FiveInchDiskImage()
+    else:
+        raise Exception("Bad disk type: %r" % physical_size)
+    return img
 
-# make destination image
-if desttype == '8': # 8"
-    destimg = imageutil.EightInchDiskImage()
-elif desttype == '5': # 5.25"
-    destimg = imageutil.FiveInchDiskImage()
-else:
-    sys.stderr.write("Bad disk type: %r" % desttype)
-    sys.exit(1)
-destfs = imageutil.Filesystem(destimg)
-destfs.format(srcfs.diskname)
+def copy_files(source_fs, destination_fs):
+    for entry in [ e for e in source_fs.read_dir() if e.used ]:
+        data = source_fs.read_file(entry.filename)
+        destination_fs.write_file(
+            filename=entry.filename,
+            filetype=entry.filetype,
+            load_address=entry.load_address,
+            entry_address=entry.entry_address,
+            data=data
+            )
 
-# copy files from source into destination
-for entry in [ e for e in srcfs.read_dir() if e.used ]:
-    data = srcfs.read_file(entry.filename)
-    destfs.write_file(
-        filename=entry.filename,
-        filetype=entry.filetype,
-        load_address=entry.load_address,
-        entry_address=entry.entry_address,
-        data=data
-        )
+if __name__ == 'main':
+    def main():
+        if len(sys.argv) != 4:
+            sys.stderr.write(__doc__)
+            sys.exit(1)
+        srcname, desttype, destname = sys.argv[1:]
 
-# write destination image
-with open(destname, 'wb') as f:
-    f.write(destimg.data)
+        # read source image
+        srcimg = read_image(srcname)
+        srcfs = imageutil.Filesystem(srcimg)
+
+        # make destination image
+        destimg = make_image(desttype)
+        destfs = imageutil.Filesystem(destimg)
+        destfs.format(srcfs.diskname)
+
+        # copy files from source into destination
+        copy_files(srcfs, destfs)
+
+        # write destination image
+        with open(destname, 'wb') as f:
+            f.write(destimg.data)
+    main()
--- tools/format.py
+++ tools/format.py
@@ -7,24 +7,28 @@ import sys
 
 import imageutil
 
-if len(sys.argv) != 3:
-    sys.stderr.write(__doc__)
-    sys.exit(1)
+def make_image(physical_size):
+    if str(physical_size)[0] == '8': # 8"
+        img = imageutil.EightInchDiskImage()
+    elif str(physical_size)[0] == '5': # 5.25"
+        img = imageutil.FiveInchDiskImage()
+    else:
+        raise Exception("Bad disk type: %r" % physical_size)
+    return img
 
-disktype, imagename = sys.argv[1:]
+if __name__ == '__main__':
+    def main():
+        if len(sys.argv) != 3:
+            sys.stderr.write(__doc__)
+            sys.exit(1)
+        disktype, imagename = sys.argv[1:]
 
-if disktype == '8': # 8"
-    img = imageutil.EightInchDiskImage()
-elif disktype == '5': # 5.25"
-    img = imageutil.FiveInchDiskImage()
-else:
-    sys.stderr.write("Bad disk type: %r" % disktype)
-    sys.exit(1)
+        # make an image and format a filesystem on it
+        img = make_image(disktype)
+        fs = imageutil.Filesystem(img)
+        fs.format(diskname=b'12345678')
 
-# format filesystem on the image
-fs = imageutil.Filesystem(img)
-fs.format(diskname=b'12345678')
-
-# write image file to disk
-with open(imagename, 'wb') as f:
-    f.write(img.data)
+        # write image file to disk
+        with open(imagename, 'wb') as f:
+            f.write(img.data)
+    main()
